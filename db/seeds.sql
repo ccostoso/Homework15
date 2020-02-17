0ed7b9be7349135f19184961479f5c2401abffed
ccostoso <carlos.costoso@outlook.com>
2020-02-17 14:48:25 -0500

    Pre removal of ORM

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('ShackBurger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Shack Stack', FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('SmokeShack', FALSE);
---
+++ public/assets/js/view.js
@@ -0,0 +1,66 @@
+$(function () {
+    console.log('this is working!');
+
+    $('#new-burger-submit').on('click', e => {
+        // Make sure to preventDefault on a submit event.
+        e.preventDefault();
+
+        const newBurger = {
+            burger_name: $("#new-burger-input").val().trim(),
+            devoured: 0
+        };
+
+        console.log("newBurger:", newBurger);
+
+        // Send the POST request.
+        $.ajax("/api/burger", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            () => {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete").on("click", function (e) {
+        const id = $(this).data("id");
+        console.log(this);
+        console.log("id:", id)
+
+        // Send the DELETE request.
+        $.ajax("/api/burger/" + id, {
+            type: "DELETE"
+        }).then(
+            () => {
+                console.log("deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".change-devoured").on("click", function (e) {
+        const id = $(this).data("id");
+        const devoured = $(this).data("devoured");
+        console.log(devoured);
+        const newDevouredState = {
+            devoured: 1
+        }
+
+        // Send the PUT request.
+        $.ajax("/api/burger/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            () => {
+                console.log("updated burger:", id);
+                console.log("new sleep state:", devoured);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+})
